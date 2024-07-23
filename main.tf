96973f14a129562aea185e3884768f097d1d0bd9
hexoren <hexoren@gmail.com>
2024-07-23 06:00:17 +0000

    add first commit:

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+	region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+	ami = "ami-0e872aee57663ae2d"
+	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.instance.id]
+	user_data = <<-EOF
+		    #!/bin/bash
+		    echo "Witaj, świecie" > index.html
+		    nohup busybox httpd -f -p 8080 &
+		    EOF
+	user_data_replace_on_change = true
+
+	tags = {
+		Name = "terraform-example"
+	}
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instance"
+	
+	ingress {
+	from_port = 8080
+	to_port = 8080
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+}
