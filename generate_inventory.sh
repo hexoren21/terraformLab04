29a1d4e1d49411f5e92ac4ee9c9428311cafbdaf
hexoren21 <hexoren@gmail.com>
2024-07-26 08:14:30 +0200

    ansible integration v.2.2

--- generate_inventory.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-terraform output -json public_ip > inventory.json
-
-jq -r '"[web]\n" + . + " ansible_ssh_user=ubuntu ansible_ssh_private_key_file=/home/trecom/.ssh/web02.pem"' inventory.json > inventory.ini
